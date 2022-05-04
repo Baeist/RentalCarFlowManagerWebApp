5539296f7a687d134d9bdde36103256f9773e916
Baeist <lars18n2@stud.kea.dk>
2022-05-04 16:37:43 +0200

    Initial

---
+++ src/main/java/com/example/rentalcarflowmanagerwebapp/RentalCarFlowManagerWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.rentalcarflowmanagerwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalCarFlowManagerWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentalCarFlowManagerWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rentalcarflowmanagerwebapp/RentalCarFlowManagerWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalcarflowmanagerwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalCarFlowManagerWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
