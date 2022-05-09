a543315c79d7534d1072b83408b80cc919c89ace
Baeist <lars18n2@stud.kea.dk>
2022-05-09 15:23:51 +0200

    create model package and more classes

---
+++ src/main/java/model/CarModel.java
@@ -0,0 +1,15 @@
+package model;
+
+public class CarModel {
+
+    private int carID;
+    private String chassisNumber;
+    private String color;
+    private String manufacturer;
+    private String carType;
+    private String carName;
+    private double carPricePerMonthDKK;
+
+    public CarModel(){}
+
+}
