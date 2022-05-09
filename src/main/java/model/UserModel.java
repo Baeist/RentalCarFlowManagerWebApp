6dbd1d78eb4b4ac87acd3ffb437aae08eae886d0
Baeist <lars18n2@stud.kea.dk>
2022-05-09 15:26:36 +0200

    add attributes to car and user model classes

--- src/main/java/model/UserModel.java
+++ src/main/java/model/UserModel.java
@@ -1,4 +1,14 @@
 package model;
 
 public class UserModel {
+
+    private int employeeID;
+    private String firstName;
+    private String lastName;
+    private String logInName;
+    private String employeePassword;
+    private String employeeType;
+
+
+
 }
