df58a2584e61d9127e50824df41ef9d93ee70624
Baeist <lars18n2@stud.kea.dk>
2022-05-09 15:13:04 +0200

    create connection for database

--- src/main/java/utility/ConnectionManager.java
+++ src/main/java/utility/ConnectionManager.java
@@ -1,4 +1,26 @@
 package utility;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionManager {
+    private static Connection connection;
+
+    private ConnectionManager() {}
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                final String DB_URL = System.getenv("url");
+                final String UID = System.getenv("user");
+                final String PWD = System.getenv("pass");
+                connection = DriverManager.getConnection(DB_URL, UID, PWD);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+
 }
