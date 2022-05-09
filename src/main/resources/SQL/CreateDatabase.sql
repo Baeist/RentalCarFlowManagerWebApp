c9a4e579b32a48173fd53f1bffd9c02f92e4cd92
Baeist <lars18n2@stud.kea.dk>
2022-05-09 12:20:39 +0200

    Add sql database and test cars and users

---
+++ src/main/resources/SQL/CreateDatabase.sql
@@ -0,0 +1,60 @@
+use rentalcarflow;
+SET foreign_key_checks = 0;
+drop table if exists employee;
+drop table if exists car;
+drop table if exists contract;
+drop table if exists lease;
+drop table if exists damage_report;
+SET foreign_key_checks = 1;
+create table if not exists employee
+(
+    employee_id   int auto_increment
+    primary key unique,
+    employee_first_name varchar(45) not null,
+    employee_last_name varchar(45) not null,
+    employee_username varchar(45) not null unique,
+    employee_password varchar(256) not null,
+    employee_type varchar(45) not null
+    );
+create table if not exists car
+(
+    car_id   int auto_increment unique
+    primary key,
+    chassis_number varchar(17) not null unique,
+    color varchar(45) not null,
+    car_manufactorer varchar(45) not null,
+    car_type varchar(45) not null,
+    car_name varchar(70) not null,
+    car_rental_price_per_month_dkk double not null
+    );
+create table if not exists contract
+(
+    contract_id   int auto_increment
+    primary key unique,
+    employee_id int not null,
+    customer_id int not null,
+    foreign key (employee_id) references employee(employee_id)
+    );
+create table if not exists lease
+(
+    lease_id   int auto_increment
+    primary key unique,
+    contract_id int not null unique,
+    car_id int not null unique,
+    lease_start_date date not null,
+    lease_period_number_of_days int not null,
+    foreign key (contract_id) references contract(contract_id),
+    foreign key (car_id) references car(car_id)
+    );
+create table if not exists damage_report
+(
+    damage_report_id   int auto_increment unique
+    primary key,
+    contract_id int not null unique,
+    car_id int not null unique,
+    dedscription varchar(400),
+    damage_price_dkk double not null,
+    garage_name varchar(45),
+    foreign key (contract_id) references contract(contract_id),
+    foreign key (car_id) references car(car_id)
+    );
---
+++ src/main/resources/SQL/CreateTestCases.sql
@@ -0,0 +1,19 @@
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '123frt123frg123fr', 'blue', 'Nissan', 'stationcar', 'NX2020', 2650);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '456frt123frg123fr', 'red', 'Nissan', 'stationcar', 'NX2020', 2650);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '789frt123frg123fr', 'black', 'Nissan', 'cabriolet', 'NX2020', 2800);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '481265t23frg123fr', 'white', 'Volvo', 'varevogn', 'NX2020', 3300);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'abcdrt123frg123fr', 'silver', 'Volvo', 'sedan', 'NX2020', 3650);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'efghrt123frg123fr', 'blue', 'Volvo', 'mini', 'NX2020', 2450);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'ihjlrt123frg123fr', 'orange', 'Toyota', 'stationcar', 'NX2020', 2550);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'iokl45123frg123fr', 'green', 'Toyota', 'stationcar', 'NX2020', 2950);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'py3frt123frg123fr', 'silver', 'Toyota', 'varevogn', 'NX2020', 3650);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '4uilrt123frg123fr', 'black', 'Toyota', 'mini', 'NX2020', 2300);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'hjkdet123frg123fr', 'blue', 'Honda', 'cabriolet', 'NX2020', 3100);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'we341t123frg123fr', 'red', 'Honda', 'sedan', 'NX2020', 2890);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'uio56t123frg123fr', 'white', 'Honda', 'cabriolet', 'NX2020', 2760);
+INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'cxe32t123frg123fr', 'grey', 'Honda', 'stationcar', 'NX2020', 2290);
+
+INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type) VALUES (default, 'Jen', 'Hansen', 'jen', '1', 'admin');
+INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type) VALUES (default, 'Dan', 'Jensen', 'dan', '2', 'dataregistrering');
+INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type) VALUES (default, 'Tim', 'Prosen', 'tim', '3', 'forretningsudvikler');
+INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type) VALUES (default, 'Pil', 'Tisen', 'pil', '4', 'skade- og mangler');
