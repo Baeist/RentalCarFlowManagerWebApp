33db8bc14f9e137a2a658bef58c978dbfd27093c
tobiasras <tobiasras.job@gmail.com>
2022-05-17 10:20:46 +0200

    changed contract changes now not unique

--- src/main/resources/SQL/CreateTestCases.sql
+++ src/main/resources/SQL/CreateTestCases.sql
@@ -1,3 +1,4 @@
+-- Cars
 INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '123frt123frg123fr', 'blue', 'Nissan', 'stationcar', 'NX2020', 2650);
 INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '456frt123frg123fr', 'red', 'Nissan', 'stationcar', 'NX2019', 2650);
 INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, '789frt123frg123fr', 'black', 'Nissan', 'cabriolet', 'NT2018', 2800);
@@ -13,9 +14,26 @@ INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_n
 INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'uio56t123frg123fr', 'white', 'Honda', 'cabriolet', 'PORPO', 2760);
 INSERT INTO car(car_id, chassis_number, color, car_manufactorer, car_type, car_name, car_rental_price_per_month_dkk) Values (default, 'cxe32t123frg123fr', 'grey', 'Honda', 'stationcar', 'RED10', 2290);
 
+-- Employee
 INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type, is_user_active) VALUES (default, 'Jen', 'Hansen', 'jen', '1', 'admin', true);
 INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type, is_user_active) VALUES (default, 'Dan', 'Jensen', 'dan', '2', 'dataregistrering', true);
 INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type, is_user_active) VALUES (default, 'Tim', 'Prosen', 'tim', '3', 'forretningsudvikler', true);
 INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type, is_user_active) VALUES (default, 'Pil', 'Tisen', 'pil', '4', 'skade- og mangler', true);
 INSERT INTO employee(employee_id, employee_first_name, employee_last_name, employee_username, employee_password, employee_type, is_user_active) VALUES (default, 'Mik', 'Dyring', 'mik', '5', 'skade- og mangler', false);
 
+-- Contracts
+INSERT INTO contract(employee_id, customer_id) VALUES (1, 1);
+INSERT INTO contract(employee_id, customer_id) VALUES (1, 2);
+INSERT INTO contract(employee_id, customer_id) VALUES (2, 3);
+INSERT INTO contract(employee_id, customer_id) VALUES (2, 4);
+INSERT INTO contract(employee_id, customer_id) VALUES (2, 5);
+INSERT INTO contract(employee_id, customer_id) VALUES (3, 6);
+INSERT INTO contract(employee_id, customer_id) VALUES (3, 6);
+INSERT INTO contract(employee_id, customer_id) VALUES (3, 7);
+INSERT INTO contract(employee_id, customer_id) VALUES (4, 8);
+INSERT INTO contract(employee_id, customer_id) VALUES (4, 9);
+INSERT INTO contract(employee_id, customer_id) VALUES (4, 10);
+INSERT INTO contract(employee_id, customer_id) VALUES (4, 11);
+
+-- Lease
+INSERT INTO lease() Values ();
