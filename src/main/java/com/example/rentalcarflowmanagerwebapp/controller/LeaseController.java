fa5919963c595169b86bf35498798dbf88870ad7
tobiasras <tobiasras.job@gmail.com>
2022-05-22 22:20:25 +0200

    Created deletede car

--- src/main/java/com/example/rentalcarflowmanagerwebapp/controller/LeaseController.java
+++ src/main/java/com/example/rentalcarflowmanagerwebapp/controller/LeaseController.java
@@ -3,6 +3,8 @@ package com.example.rentalcarflowmanagerwebapp.controller;
 import com.example.rentalcarflowmanagerwebapp.model.Car;
 import com.example.rentalcarflowmanagerwebapp.model.Contract;
 import com.example.rentalcarflowmanagerwebapp.model.Lease;
+import com.example.rentalcarflowmanagerwebapp.repository.CarRepository;
+import com.example.rentalcarflowmanagerwebapp.service.CarService;
 import com.example.rentalcarflowmanagerwebapp.service.ContractService;
 import com.example.rentalcarflowmanagerwebapp.service.LeaseService;
 import org.springframework.stereotype.Controller;
@@ -19,10 +21,12 @@ public class LeaseController {
 
     private LeaseService leaseService;
     private ContractService contractService;
+    private CarService carService;
 
-    public LeaseController(LeaseService leaseService, ContractService contractService) {
+    public LeaseController(LeaseService leaseService, ContractService contractService, CarService carService) {
         this.leaseService = leaseService;
         this.contractService = contractService;
+        this.carService = carService;
     }
 
     @GetMapping("/lease")
@@ -48,12 +52,15 @@ public class LeaseController {
                             @RequestParam("carID") int carID
                             ){
 
+
+
+
         LocalDate startLocalDate = leaseService.convertStringtoLocalDate(startDate);
 
         Lease lease = new Lease(leaseID, startLocalDate, contractID, periodDays, carID);
         leaseService.saveLease(lease);
 
-        return "redirect:/dashboard/lease";
+        return "redirect:/lease";
     }
 
     @GetMapping("/lease/delete/{leaseID}")
@@ -62,16 +69,41 @@ public class LeaseController {
         return "redirect:/lease";
     }
 
-    @GetMapping("/lease/edit/{leaseID}")
-    public String editLease(@PathVariable("leaseID") int leaseID, Model model){
+    @GetMapping("/lease/edit/{contractID}/{leaseID}/{leaseStartDate}/{totalDays}/{carID}")
+    public String editLease(@PathVariable("leaseID") int leaseID,
+                            @PathVariable("leaseStartDate") String date,
+                            @PathVariable("totalDays") int totalDays,
+                            @PathVariable("carID") int carID,
+                            @PathVariable("contractID") int contractID,
+                            Model model){
+
+        List<Contract> allContracts = contractService.GetAllContracts();
+        List<Car> allAvailableCars = carService.availableCars();
+
+        model.addAttribute("allContracts", allContracts);
+        model.addAttribute("allAvailableCars", allAvailableCars);
+
         model.addAttribute("pageTitle", "Ændrer lease: " + leaseID);
+
         model.addAttribute("leaseID", leaseID);
+        model.addAttribute("leaseStartDate", date);
+        model.addAttribute("totalDays", totalDays);
+        model.addAttribute("carID", carID);
+        model.addAttribute("contractID", contractID);
+
         return "Forms/lease_form";
     }
 
     @GetMapping("/lease/create")
     public String createLease(Model model){
         model.addAttribute("pageTitle", "Opret lejeaftale");
+
+        List<Contract> allContracts = contractService.GetAllContracts();
+        List<Car> allAvailableCars = carService.availableCars();
+
+        model.addAttribute("allContracts", allContracts);
+        model.addAttribute("allAvailableCars", allAvailableCars);
+
 
         // gives none existing id
         model.addAttribute("leaseID", -1);
